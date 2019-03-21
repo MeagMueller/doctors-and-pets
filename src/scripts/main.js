3e7f5a32b6a7f30e681b29d35ade4e18f6eb8948
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-03-21 14:49:19 -0500

    Finished pet exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,23 @@
-const doctorInfo = function (doctorName, specialty, address) {
-    return {
-        "name": doctorName,
-        "specialty": specialty,
-        "address": address
+// const doctorInfo = function (doctorName, specialty, address) {
+//     return {
+//         "name": doctorName,
+//         "specialty": specialty,
+//         "address": address
 
+//     }
+// }
+
+// const thisDoctor = doctorInfo("Doctor Mueller", "Psychiatry", "123 This Place")
+
+// console.log(thisDoctor)
+
+const pet = function (petName, petBreed) {
+    return {
+        "name": petName,
+        "breed": petBreed
     }
 }
 
-const thisDoctor = doctorInfo("Doctor Mueller", "Psychiatry", "123 This Place")
+const thisPet = pet("Fluffernutter", "Giant Floof")
 
-console.log(thisDoctor)
+console.log(thisPet)
